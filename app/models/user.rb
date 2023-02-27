8fc231f45e48b0c43dac19a626ac69443d881a37
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-02-27 16:09:48 -0600

    2-27-23

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
-         :recoverable, :rememberable, :validatable
-=======
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
->>>>>>> 4157ddc8985f4af9aadf09b27844de6037dc220c
 end
