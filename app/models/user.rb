87767d63ba7edc65b3ea0030f3e04b1cfa5fec1a
theGeekkit <sonofgondolin16@gmail.com>
2023-03-01 19:37:20 -0600

    who knows

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
+
+def display_name
+  user_name || email
+end
+
 end
